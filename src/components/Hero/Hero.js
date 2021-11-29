07cce82f57fc02558c081e8a8767fe05861371ab
tahidul1 <contact.tahidul@gmail.com>
2021-11-29 23:57:26 +0600

    update

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <section className="hero">
+            <div className="container">
+                <article>
+                    <p className="hero__lead">Hi, my name is</p>
+                    <h1 className="hero__title">Tahidul Islam</h1>
+                    <h2 className="hero__title">I build things for the web.</h2>
+                    <p className="hero_text">I’m a software engineer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I’m focused on building accessible, human-centered products at Upstatement.</p>
+                </article>
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
