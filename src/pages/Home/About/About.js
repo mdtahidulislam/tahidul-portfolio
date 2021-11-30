e24cf4e88544dac076e07ed3eb9830a2763f6136
tahidul1 <contact.tahidul@gmail.com>
2021-11-30 22:22:03 +0600

    done about section

--- src/pages/Home/About/About.js
+++ src/pages/Home/About/About.js
@@ -14,6 +14,16 @@ const About = () => {
                         <p>Fast-forward to today, and I’ve had the privilege of working at an advertising agency, a start-up, a huge corporation, and a student-led design studio. My main focus these days is building accessible, inclusive products and digital experiences at Upstatement for a variety of clients.</p>
                         <p>I also recently launched a course that covers everything you need to build a web app with the Spotify API using Node & React.</p>
                         <p>Here are a few technologies I’ve been working with recently:</p>
+                        <ul className="skill-set">
+                            <li>JavaScript (ES6+)</li>
+                            <li>React</li>
+                            <li>Node.js</li>
+                            <li>Shopify</li>
+                            <li>WordPress</li>
+                            <li>Material UI</li>
+                            <li>Tailwind CSS</li>
+                            <li>Bootstrap</li>
+                        </ul>
                     </article>
                     <div className="about-img">
                         <img src={imgMine} alt="Tahidul" />
